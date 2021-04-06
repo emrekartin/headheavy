7890ff12e0ec468a098c709284ffbf0c31508a89
emrekartin <emrekartin@outlook.com>
2021-04-07 02:31:49 +0300

    v0.2

---
+++ Assets/Scripts/pot.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+using UnityEngine.SceneManagement;
+public class pot : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject.Find("ground (1)").GetComponent<BoxCollider2D>().isTrigger = false;
+        StartCoroutine(WaitForLevelSwitch());
+    }
+
+    IEnumerator WaitForLevelSwitch()
+    {
+        yield return new WaitForSeconds(1);
+        GameObject.Find("result").GetComponent<TextMesh>().text = "Excellent";
+        GameObject.Find("result").GetComponent<MeshRenderer>().enabled = true;
+        
+        
+        StartCoroutine(WaitForLevelSwitch1());
+    }
+    IEnumerator WaitForLevelSwitch1()
+    {
+        yield return new WaitForSeconds(4);
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
